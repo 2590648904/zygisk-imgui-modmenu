f709e2dc04fa5e099841e83274446f967f897885
2590648904 <166786720+2590648904@users.noreply.github.com>
2024-05-27 14:34:14 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.catsbit.sandbox3d"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
